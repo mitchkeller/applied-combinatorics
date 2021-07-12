c6258f80b4ab998b8e359c31180476b25110ddcb
mitchkeller <mitchkeller@users.noreply.github.com>
2021-07-12 15:44:43 -0500

    Updates to build process for 2021 PTX

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,8 @@ case $i in
     -f=*|--format=*) # Any string beginning with h or H: HTML
 	# Any string beginning with l or L: LaTeX
 	# (including calling pdflatex)
+	# Any string beginning with e or E: EPUB (with SVG math)
+	# Any string beginning with k or K: Kindle (EPUB with MathML math)
 	# Any other string: Error
     FORMAT="${i#*=}"
     shift # past argument=value
@@ -34,15 +36,36 @@ ACDIR=`pwd`
 case $FORMAT in
     [hH]*)
 	cp ${ACDIR}/xsl/applied-combinatorics-html.xsl ${PTX}/user
-	cd ${OUTDIR}
-	xsltproc -stringparam publisher ${PUBFILE} -xinclude ${PTX}/user/applied-combinatorics-html.xsl ${ACDIR}/src/min-index.ptx
+	rm -rf ${OUTDIR}
+	install -d ${OUTDIR}
+	${PTX}/pretext/pretext -c all -f html -o ${OUTDIR} -x\
+	      exercise.divisional.statement yes exercise.divisional.hint yes\
+	      exercise.divisional.answer no\
+	      exercise.divisional.solution no -p ${PUBFILE}\
+	      -d ${OUTDIR} ${ACDIR}/src/index.ptx 
 	;;
     [lL]*)
+	install -d ${OUTDIR}
 	cd ${OUTDIR}
 	xsltproc -o IBL-modernalgebra.tex -stringparam publisher ${PUBFILE} -xinclude ${PTX}/xsl/mathbook-latex.xsl ${ACDIR}/ptx/index.ptx
 	xelatex app-comb
 	xelatex app-comb
 	xelatex app-comb
+	;;
+    [eE]*)
+	install -d ${OUTDIR}	
+	${PTX}/pretext/pretext -c all -f epub-svg -p ${PUBFILE} -x\
+	      exercise.divisional.statement yes exercise.divisional.hint yes\
+	      exercise.divisional.answer no\
+	      exercise.divisional.solution no -d ${OUTDIR} -o appcomb-svg.epub ${ACDIR}/src/index.ptx
+	;;
+    [kK]*)
+	install -d ${OUTDIR}	
+	cd ${OUTDIR}
+	${PTX}/pretext/pretext -c all -f epub-kindle -p ${PUBFILE} -x\
+	      exercise.divisional.statement yes exercise.divisional.hint yes\
+	      exercise.divisional.answer no\
+	      exercise.divisional.solution no -d ${OUTDIR} -o appcomb-kindle.epub ${ACDIR}/src/index.ptx
 	;;
     *)
 	echo "Invalid format: ${FORMAT}"
