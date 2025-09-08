af8e748fdc083de475fe2a1d2b48daa006c9f2e3
Mitch Keller <mitchkeller@users.noreply.github.com>
2025-09-08 19:41:25 +0000

    Latest build deployed.

--- activities/lunr-pretext-search-index.js
+++ activities/lunr-pretext-search-index.js
@@ -13,7 +13,7 @@ var ptx_lunr_docs = [
   "id": "ws-day_one-3",
   "level": "2",
   "url": "ws-day_one.html#ws-day_one-3",
-  "type": "Worksheet Exercise",
+  "type": "Problem",
   "number": "1.1.1",
   "title": "",
   "body": "  Read the list of seven questions involving Amanda, Dawn, Keesha, and Seth in Section 1.2 Enumeration. As you read the problems, discuss them to ensure that you understand what the question is asking. The discuss each of the questions below.     Compare the first two questions. Do you think one of these must have a bigger answer, or are they asking the same question? Explain your reasoning.      Discuss the third question. Do you have a consensus on how this changes the answer(s)?      Is the scenario in question 4 different to that in questions 1 and 2? Explain your reasoning.      Compare the first fourth and fifth questions. Do you think one of these must have a bigger answer, or are they asking the same question? Explain your reasoning.      Discuss the sixth question. Do you have a consensus on how this changes the answer(s)?      Discuss the seventh question. You might think in terms of growth factor here: If we increase the number of people by multiplying by , how (approximately) might you expect the answers to increase? Can we even make a reasonable guess?     We will not have time in this course to discuss the necklace-type counting problems introduced at the end of the section. You're welcome to read those questions and discuss them today if you're interested, but you'll have to do some extra reading after the semester to study this topic.   "
@@ -22,7 +22,7 @@ var ptx_lunr_docs = [
   "id": "ws-day_one-4",
   "level": "2",
   "url": "ws-day_one.html#ws-day_one-4",
-  "type": "Worksheet Exercise",
+  "type": "Problem",
   "number": "1.1.2",
   "title": "",
   "body": "  In Section 1.3 Combinatorics and Graph Theory, we endeavor to show how intuitive many of the ideas in combinatorics can be. We will define the terms of this section precisely in Chapter 5, so don't worry when we move on today without a careful understanding of things!     Discuss the 11 items after Figure 1.2. Which of these can you understand from the context given? What things remain unclear or unambiguous in a way that suggests we will want more precise definitions later on?      Discuss the five questions of Example 1.3. Are there any you can confidently answer today? What does your gut tell you about how easy or hard it would be for a computer algorithm to answer these questions efficiently?      For Example 1.5, spend a few minutes trying to answer the questions presented after Figure 1.6, which is reproduced below so you can draw on it if you'd like.   24 points labeled with integers as well as a scale        Discuss Example 1.7 briefly. Does this seem to be a hard problem or an easy one?    "
@@ -31,7 +31,7 @@ var ptx_lunr_docs = [
   "id": "ws-day_one-5",
   "level": "2",
   "url": "ws-day_one.html#ws-day_one-5",
-  "type": "Worksheet Exercise",
+  "type": "Problem",
   "number": "1.1.3",
   "title": "",
   "body": "  In Section 1.4 Combinatorics and Number Theory, you get to experience a preview of some of the integrated interactivity our textbook provides. Code in our text uses SageMath, an open source computer algebra system that uses Python as its underlying syntax.     Discuss Example 1.8 very briefly and resist the siren song of the Collatz conjecture.      What do you notice when running the SageMath code in Example 1.9? What do you wonder about?      Look over the partitions in Figure 1.11. What do you notice? What do you wonder? If time permits after you complete the other questions on this worksheet, come back and start making a list of the partitions of , identifying those into distinct parts and those into odd parts.    "
@@ -40,10 +40,136 @@ var ptx_lunr_docs = [
   "id": "ws-day_one-6",
   "level": "2",
   "url": "ws-day_one.html#ws-day_one-6",
-  "type": "Worksheet Exercise",
+  "type": "Problem",
   "number": "1.1.4",
   "title": "",
   "body": "  We conclude our course overview with Section 1.5 Combinatorics and Geometry, which includes some topics we will sample from throughout the course.     In Example 1.12, I suggest starting with fewer lines. How many regions would be determined by one, two, or three lines? Can you draw five lines that meet the criteria given in this example?      For Example 1.15, work to try to redraw the graph in such a way that no edges cross. If you can't redraw it with no crossing edges, do you think that this is simply an impossible task? What do you think about the question the text asks about Sam and Deborah's discussion?    "
+},
+{
+  "id": "ws-strings-perm-comb",
+  "level": "1",
+  "url": "ws-strings-perm-comb.html",
+  "type": "Worksheet",
+  "number": "2.1",
+  "title": "Strings, Permutations, Binomial Coefficients",
+  "body": " Strings, Permutations, Binomial Coefficients    Suppose a state issues license plates that are numbered with strings of length so that the first three characters must be upper-case letters in the English alphabet and the last three characters must be base digits.     How many license plates are possible?           Suppose the state starts suffering confusion between and .     How many license plates are possible if they prohibit the use of the letter in the alphabetic part?      What if they allow but prohibit ?      What about prohibiting both and ?        How many -digit positive integers are there? (Assume that we count by writing an integer using as few digits as possible so that is a -digit integer but not a -digit integer.) Make your count in two ways:     By figuring out the smallest and largest integers you want to count and determining how many there must therefore be.      By considering -digit positive integers as strings.       How many -digit positive integers are divisible by ?      How many -digit positive integers are divisible by ?      Let's go back to license plates allowing any three letters followed by any three numbers.     How many license plates start with a vowel followed by a consonant?      How many license plates end with a -digit number divisible by ? (Here we allow leading zeros in -digit numbers, unlike in the previous three questions.)      How many license plates start with a vowel followed by a consonant and end with a -digit number divisible by ?      How many license plates start with a vowel followed by a consonant or end with a -digit number divisible by ?       A scholarship selection committee has been tasked with choosing five winners from applicants. The winners will receive scholarships of $10,000, $8,000, $6,000, $4,000, and $2,000. How many possible outcomes are there from the scholarship competition?      In this question, -digit is defined as in above.     How many -digit positive integers are there with all distinct digits?      How many -digit positive integers with distinct digits are divisible by ?       Let's change the scholarship problem from . Suppose the committee is instead tasked with choosing five winners (from applicants), each of whom will receive a $6,000 scholarship. Is the number of possible outcomes here less than, the same as, or greater than the number of ways to award scholarships of $10,000, $8,000, $6,000, $4,000, and $2,000?      How many binary strings of length are there containing exactly three s?      Suppose a basketball team has players.     How many ways are there to choose the players who are on the court at any given time?      To be a bit more realistic, suppose there are players who play guard, players who play forward, and players who play center. The team's offense calls for two guards, two forwards, and one center to be on the court at all times. How many ways are there for the coach to pick the players to put in the game in this scenario?       Alice's Diner is a meat and three restaurant at which customers order a plate consisting of one of five meats and three sides chosen from a list of options. How many different plates can be ordered? What if Alice's Diner offers a deluxe plate offering the customer's choice of two meats (but still three sides)?      Suppose we're making six-character license plates using uppercase letters and digits.     How many license plates are possible if we require the license plate to contain exactly two W's, which are allowed to appear in any of the six positions, and then can fill the other four positions with any digit or letter (other than W)?      How about if we modify the scenario above by not allowing any character to be repeated when filling the other four positions?      Suppose we want to keep the requirement of exactly three letters and exactly three digits (repetition allowed for both) but no longer want to mandate which positions are letters and which positions are digits. How many license plates can we make now?       Come up with a simple value for the sum .    Start by trying it out for small values of . Maybe think about binary strings to prove your conjecture true.    "
+},
+{
+  "id": "ws-strings-perm-comb-2",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-2",
+  "type": "Problem",
+  "number": "2.1.1",
+  "title": "",
+  "body": "  Suppose a state issues license plates that are numbered with strings of length so that the first three characters must be upper-case letters in the English alphabet and the last three characters must be base digits.     How many license plates are possible?           Suppose the state starts suffering confusion between and .     How many license plates are possible if they prohibit the use of the letter in the alphabetic part?      What if they allow but prohibit ?      What about prohibiting both and ?     "
+},
+{
+  "id": "wkst-digit",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#wkst-digit",
+  "type": "Problem",
+  "number": "2.1.2",
+  "title": "",
+  "body": "  How many -digit positive integers are there? (Assume that we count by writing an integer using as few digits as possible so that is a -digit integer but not a -digit integer.) Make your count in two ways:     By figuring out the smallest and largest integers you want to count and determining how many there must therefore be.      By considering -digit positive integers as strings.    "
+},
+{
+  "id": "ws-strings-perm-comb-4",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-4",
+  "type": "Problem",
+  "number": "2.1.3",
+  "title": "",
+  "body": "  How many -digit positive integers are divisible by ?   "
+},
+{
+  "id": "ws-strings-perm-comb-5",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-5",
+  "type": "Problem",
+  "number": "2.1.4",
+  "title": "",
+  "body": "  How many -digit positive integers are divisible by ?   "
+},
+{
+  "id": "ws-strings-perm-comb-6",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-6",
+  "type": "Problem",
+  "number": "2.1.5",
+  "title": "",
+  "body": "  Let's go back to license plates allowing any three letters followed by any three numbers.     How many license plates start with a vowel followed by a consonant?      How many license plates end with a -digit number divisible by ? (Here we allow leading zeros in -digit numbers, unlike in the previous three questions.)      How many license plates start with a vowel followed by a consonant and end with a -digit number divisible by ?      How many license plates start with a vowel followed by a consonant or end with a -digit number divisible by ?    "
+},
+{
+  "id": "wkst-scholarship",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#wkst-scholarship",
+  "type": "Problem",
+  "number": "2.1.6",
+  "title": "",
+  "body": "  A scholarship selection committee has been tasked with choosing five winners from applicants. The winners will receive scholarships of $10,000, $8,000, $6,000, $4,000, and $2,000. How many possible outcomes are there from the scholarship competition?   "
+},
+{
+  "id": "ws-strings-perm-comb-8",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-8",
+  "type": "Problem",
+  "number": "2.1.7",
+  "title": "",
+  "body": "  In this question, -digit is defined as in above.     How many -digit positive integers are there with all distinct digits?      How many -digit positive integers with distinct digits are divisible by ?    "
+},
+{
+  "id": "ws-strings-perm-comb-9",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-9",
+  "type": "Problem",
+  "number": "2.1.8",
+  "title": "",
+  "body": "  Let's change the scholarship problem from . Suppose the committee is instead tasked with choosing five winners (from applicants), each of whom will receive a $6,000 scholarship. Is the number of possible outcomes here less than, the same as, or greater than the number of ways to award scholarships of $10,000, $8,000, $6,000, $4,000, and $2,000?   "
+},
+{
+  "id": "ws-strings-perm-comb-10",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-10",
+  "type": "Problem",
+  "number": "2.1.9",
+  "title": "",
+  "body": "  How many binary strings of length are there containing exactly three s?   "
+},
+{
+  "id": "ws-strings-perm-comb-11",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-11",
+  "type": "Problem",
+  "number": "2.1.10",
+  "title": "",
+  "body": "  Suppose a basketball team has players.     How many ways are there to choose the players who are on the court at any given time?      To be a bit more realistic, suppose there are players who play guard, players who play forward, and players who play center. The team's offense calls for two guards, two forwards, and one center to be on the court at all times. How many ways are there for the coach to pick the players to put in the game in this scenario?    "
+},
+{
+  "id": "ws-strings-perm-comb-12",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-12",
+  "type": "Problem",
+  "number": "2.1.11",
+  "title": "",
+  "body": "  Alice's Diner is a meat and three restaurant at which customers order a plate consisting of one of five meats and three sides chosen from a list of options. How many different plates can be ordered? What if Alice's Diner offers a deluxe plate offering the customer's choice of two meats (but still three sides)?   "
+},
+{
+  "id": "ws-strings-perm-comb-13",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-13",
+  "type": "Problem",
+  "number": "2.1.12",
+  "title": "",
+  "body": "  Suppose we're making six-character license plates using uppercase letters and digits.     How many license plates are possible if we require the license plate to contain exactly two W's, which are allowed to appear in any of the six positions, and then can fill the other four positions with any digit or letter (other than W)?      How about if we modify the scenario above by not allowing any character to be repeated when filling the other four positions?      Suppose we want to keep the requirement of exactly three letters and exactly three digits (repetition allowed for both) but no longer want to mandate which positions are letters and which positions are digits. How many license plates can we make now?    "
+},
+{
+  "id": "ws-strings-perm-comb-14",
+  "level": "2",
+  "url": "ws-strings-perm-comb.html#ws-strings-perm-comb-14",
+  "type": "Problem",
+  "number": "2.1.13",
+  "title": "",
+  "body": "  Come up with a simple value for the sum .    Start by trying it out for small values of . Maybe think about binary strings to prove your conjecture true.   "
 }
 ]
 
